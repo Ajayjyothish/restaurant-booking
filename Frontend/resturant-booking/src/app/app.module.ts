341cba4e3adf73c3bceb869d7b726aab347c8fcd
ajayj <ajayj@qburst.com>
2020-12-01 12:28:01 +0530

    modularized homepage

--- Frontend/resturant-booking/src/app/app.module.ts
+++ Frontend/resturant-booking/src/app/app.module.ts
@@ -1,23 +1,18 @@
 import { AuthService } from './auth.service';
-import { SignupComponent } from './signup/signup.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FooterComponent } from './footer/footer.component';
 
 import { RestaurantsService } from './restaurants.service';
-import { RestaurantCardsComponent } from './restaurant-cards/restaurant-cards.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { ValidateEqualModule } from 'ng-validate-equal';
-import { LoginComponent } from './login/login.component';
 
 import { AuthInterceptor } from './AuthInterceptor';
-import { ForgotpassComponent } from './forgotpass/forgotpass.component';
 import { PasswordResetComponent } from './password-reset/password-reset.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RestaurantListingPageComponent } from './restaurant-listing-page/restaurant-listing-page.component';
@@ -29,12 +24,7 @@ import { EditProfileComponent } from './edit-profile/edit-profile.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     FooterComponent,
-    RestaurantCardsComponent,
-    SignupComponent,
-    LoginComponent,
-    ForgotpassComponent,
     PasswordResetComponent,
     NavbarComponent,
     RestaurantListingPageComponent,
@@ -47,9 +37,8 @@ import { EditProfileComponent } from './edit-profile/edit-profile.component';
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    HttpClientModule,
     NgbModule,
-    ReactiveFormsModule,
+    HttpClientModule,
     ValidateEqualModule
   ],
   providers: [RestaurantsService, AuthService, {
